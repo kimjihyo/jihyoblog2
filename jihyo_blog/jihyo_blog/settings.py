c9d8c0760068eea5d56ad547a0a23dfe8a44f679
Jihyo Kim <kimjihyo0325@gmail.com>
2019-01-09 21:46:54 -0800

    django-heroku

--- jihyo_blog/jihyo_blog/settings.py
+++ jihyo_blog/jihyo_blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +125,5 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+
+django_heroku.settings(locals())
